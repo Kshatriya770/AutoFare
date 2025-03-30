e4ba1cd9d9ad2fd628ac194b4304baa2314ebf89
Saurabh Singh <35197439+Kshatriya770@users.noreply.github.com>
2025-03-30 20:44:29 +0530

    main activity

--- app/src/main/java/com/lxndxn/autofare/MainActivity.kt
+++ app/src/main/java/com/lxndxn/autofare/MainActivity.kt
@@ -16,17 +16,7 @@ import com.lxndxn.autofare.ui.theme.AutoFareTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            AutoFareTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
-        }
+        setContentView(R.layout.activity_main)
     }
 }
 
